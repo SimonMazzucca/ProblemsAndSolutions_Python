9a1b4ce72fb33ea61216896549039ec25d08a029
Simon Mazzucca <simonmazzucca@hotmail.com>
2020-11-09 08:49:13 -0500

    LC0001_Two_Sum: stub

---
+++ leetcode/problems/lc0001_two_sum.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class LC0001_Two_Sum:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return [0, 1]
---
+++ leetcode/unit_tests/test_lc0001_two_sum.py
@@ -0,0 +1,13 @@
+import unittest
+from leetcode.problems.lc0001_two_sum import LC0001_Two_Sum
+
+
+class LC0001_Two_Sum_Tests(unittest.TestCase):
+
+    def test_LC0001_Two_Sum_Test_Case_01(self):
+        lc = LC0001_Two_Sum()
+        nums = [2, 7, 11, 15]
+        expected = [0, 1]
+        actual = lc.twoSum(nums, 9)
+
+        self.assertEqual(expected, actual)
