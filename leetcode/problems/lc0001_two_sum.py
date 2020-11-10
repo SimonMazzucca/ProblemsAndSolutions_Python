73131e8f89d0fdce919bf0df0b6b6c1354ffb0dc
Simon Mazzucca <simonmazzucca@hotmail.com>
2020-11-10 07:02:12 -0500

    LC0001_Two_Sum: done

--- leetcode/problems/lc0001_two_sum.py
+++ leetcode/problems/lc0001_two_sum.py
@@ -3,4 +3,14 @@ from typing import List
 
 class LC0001_Two_Sum:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return [0, 1]
+        indices = {}
+
+        for i in range(len(nums)):
+            indices[nums[i]] = i
+
+        for i in range(len(nums)):
+            n2 = target - nums[i]
+            if n2 in indices and indices[n2] != i:
+                return [i, indices[n2]]
+
+        return []
--- leetcode/unit_tests/test_lc0001_two_sum.py
+++ leetcode/unit_tests/test_lc0001_two_sum.py
@@ -11,3 +11,19 @@ class LC0001_Two_Sum_Tests(unittest.TestCase):
         actual = lc.twoSum(nums, 9)
 
         self.assertEqual(expected, actual)
+
+    def test_LC0001_Two_Sum_Test_Case_02(self):
+        lc = LC0001_Two_Sum()
+        nums = [3, 2, 4]
+        expected = [1, 2]
+        actual = lc.twoSum(nums, 6)
+
+        self.assertEqual(expected, actual)
+
+    def test_LC0001_Two_Sum_Test_Case_03(self):
+        lc = LC0001_Two_Sum()
+        nums = [3, 3]
+        expected = [0, 1]
+        actual = lc.twoSum(nums, 6)
+
+        self.assertEqual(expected, actual)
